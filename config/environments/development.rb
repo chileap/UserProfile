d4f81387f08d0c6c4a79131df7ac41627091fe2e
chileap <chileapchhin@gmail.com>
2016-01-24 10:53:41 +0700

    User login and Sign up with facebook and devise

---
+++ config/environments/development.rb
@@ -0,0 +1,17 @@
+Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host:'localhost:3000' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => 'localhost:3000',
+      :user_name => "chileapchhin@gmail.com",
+      :password => "12021995",
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
+  
+end
